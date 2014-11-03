b9e56dbe104c2d569c0714b02aa5ecd663f6041e
torberglund <tor.berglund@gmail.com>
2014-11-03 16:39:33 +0100

    Inserted inquiry regarding name

--- helloworld.java
+++ helloworld.java
@@ -1,7 +1,14 @@
+import java.util.Scanner;
 class helloworld {
 
+static String s;
+
 public static void main (String []arg) {
+	Scanner scan = new Scanner(System.in);
 	System.out.println("Hello World");
+	System.out.print("What is your name? "); System.out.flush();
+	s = scan.nextLine();
+	System.out.println("Hello "+ s + "!");
 
 	};
 };
